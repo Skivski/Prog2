e6b1a4b6eca77737f4a27018176d7440cbd1764f
Anton Frost <anton.frost.5869@student.uu.se>
2022-05-20 00:01:24 +0200

    19.4

---
+++ MA4_files/fibs.py
@@ -0,0 +1,24 @@
+from integer import Integer
+
+
+
+f = Integer(7)
+n = f.get()
+
+def fib_pure_py(n):
+    if n <= 1:
+        return n
+    else:
+        return(fib_pure_py(n-1) + fib_pure_py(n-2))
+
+print(fib_pure_py(n))
+
+#@njit
+def fib_numba_py(n):
+    if n <= 1:
+        return n
+    else:
+        return(fib_numba_py(n-1) + fib_numba_py(n-2))
+
+print(fib_numba_py(n))
+
--- MA4_files/pyfib.py
+++
@@ -1,7 +0,0 @@
-# Pure Python Fibonacci.
-
-def fib_py(n):
-    if n <= 1:
-        return n
-    else:
-        return(fib_py(n-1) + fib_py(n-2))
